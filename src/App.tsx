f81937f0a0b2a4282d26a0ced03c7b6317f327a2
carlosjr-dev <wolfnygard@gmail.com>
2023-09-02 22:58:50 -0400

    pagehome

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Ask from "./pages/ask/Ask";
-import EndGame from "./pages/end/EndGame";
-import HomePage from "./pages/home/HomePage";
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Ask />}></Route>
-        <Route path="/quest" element={<HomePage />}></Route>
-        <Route path="/endgame" element={<EndGame />}></Route>
       </Routes>
     </BrowserRouter>
   );
